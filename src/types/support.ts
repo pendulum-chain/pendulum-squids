45a82f3e75ab9645324de010a385648a6453ef22
Marcel Ebert <mail@marcel-ebert.de>
2023-04-21 18:42:05 +0200

    Re-generate types

--- src/types/support.ts
+++ src/types/support.ts
@@ -23,7 +23,11 @@ export interface Chain {
     decodeCall(call: Call): any
     getStorageItemTypeHash(prefix: string, name: string): string | undefined
     getStorage(blockHash: string, prefix: string, name: string, ...args: any[]): Promise<any>
-    queryStorage(blockHash: string, prefix: string, name: string, ...args: any[]): Promise<any[]>
+    queryStorage2(blockHash: string, prefix: string, name: string, keyList?: any[]): Promise<any[]>
+    getKeys(blockHash: string, prefix: string, name: string, ...args: any[]): Promise<any[]>
+    getPairs(blockHash: string, prefix: string, name: string, ...args: any[]): Promise<any[]>
+    getKeysPaged(pageSize: number, blockHash: string, prefix: string, name: string, ...args: any[]): AsyncIterable<any[]>
+    getPairsPaged(pageSize: number, blockHash: string, prefix: string, name: string, ...args: any[]): AsyncIterable<[key: any, value: any][]>
     getConstantTypeHash(pallet: string, name: string): string | undefined
     getConstant(pallet: string, name: string): any
 }
@@ -63,22 +67,6 @@ export interface BlockContext extends ChainContext {
 
 export interface Block {
     hash: string
-}
-
-export interface Chain {
-    getEventHash(eventName: string): string
-    decodeEvent(event: Event): any
-    getCallHash(name: string): string
-    decodeCall(call: Call): any
-    getStorageItemTypeHash(prefix: string, name: string): string | undefined
-    getStorage(blockHash: string, prefix: string, name: string, ...args: any[]): Promise<any>
-    queryStorage2(blockHash: string, prefix: string, name: string, keyList?: any[]): Promise<any[]>
-    getKeys(blockHash: string, prefix: string, name: string, ...args: any[]): Promise<any[]>
-    getPairs(blockHash: string, prefix: string, name: string, ...args: any[]): Promise<any[]>
-    getKeysPaged(pageSize: number, blockHash: string, prefix: string, name: string, ...args: any[]): AsyncIterable<any[]>
-    getPairsPaged(pageSize: number, blockHash: string, prefix: string, name: string, ...args: any[]): AsyncIterable<[key: any, value: any][]>
-    getConstantTypeHash(pallet: string, name: string): string | undefined
-    getConstant(pallet: string, name: string): any
 }
 
 
--- src/types/v7.ts
+++ src/types/v7.ts
@@ -1,5 +1,11 @@
 import type {Result, Option} from './support'
 
+export interface AssetId {
+    chainId: number
+    assetType: number
+    assetIndex: bigint
+}
+
 export type PairStatus = PairStatus_Trading | PairStatus_Bootstrap | PairStatus_Disable
 
 export interface PairStatus_Trading {
@@ -9,7 +15,7 @@ export interface PairStatus_Trading {
 
 export interface PairStatus_Bootstrap {
     __kind: 'Bootstrap'
-    value: BootstrapParamter
+    value: BootstrapParameter
 }
 
 export interface PairStatus_Disable {
@@ -18,12 +24,12 @@ export interface PairStatus_Disable {
 
 export interface PairMetadata {
     pairAccount: Uint8Array
-    targetSupply: bigint
+    totalSupply: bigint
 }
 
-export interface BootstrapParamter {
-    minContribution: [bigint, bigint]
+export interface BootstrapParameter {
     targetSupply: [bigint, bigint]
+    capacitySupply: [bigint, bigint]
     accumulatedSupply: [bigint, bigint]
     endBlockNumber: number
     pairAccount: Uint8Array

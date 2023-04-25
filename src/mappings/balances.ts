34ed90cf10a4704ec2c6ca31e19ad89b1c338af2
Marcel Ebert <mail@marcel-ebert.de>
2023-04-25 19:49:03 +0200

    Extract logic for the balance transfer event

---
+++ src/mappings/balances.ts
@@ -0,0 +1,26 @@
+import { codec } from '@subsquid/ss58'
+import { EventHandlerContext } from '../types'
+import { Account, Mint, Transfer } from '../model'
+import { BalancesTransferEvent } from '../types/events'
+import { config } from '../config'
+
+export async function handleBalanceTransfer(ctx: EventHandlerContext) {
+    const _event = new BalancesTransferEvent(ctx, ctx.event)
+    const event = _event.asV1
+
+    let from = codec(config.prefix).encode(event.from)
+    let to = codec(config.prefix).encode(event.to)
+
+    const transfer = new Transfer({
+        id: ctx.event.id,
+        blockNumber: ctx.block.height,
+        timestamp: new Date(ctx.block.timestamp),
+        extrinsicHash: ctx.event.extrinsic?.hash,
+        from,
+        to,
+        amount: event.amount,
+        fee: ctx.event.extrinsic?.fee || 0n,
+    })
+
+    await ctx.store.save(transfer)
+}
--- src/processor.ts
+++ src/processor.ts
@@ -1,14 +1,9 @@
-import { lookupArchive } from '@subsquid/archive-registry'
-import * as ss58 from '@subsquid/ss58'
 import {
     BatchContext,
     BatchProcessorItem,
     SubstrateBatchProcessor,
 } from '@subsquid/substrate-processor'
 import { Store, TypeormDatabase } from '@subsquid/typeorm-store'
-import { In } from 'typeorm'
-import { Account, Transfer } from './model'
-import { BalancesTransferEvent } from './types/events'
 import { config } from './config'
 import {
     handleAssetSwap,
@@ -21,11 +16,13 @@ import {
     handleTokenWithdrawn,
 } from './mappings/token'
 import { TOKEN_EVENT_TYPE } from './types'
+import { handleBalanceTransfer } from './mappings/balances'
 
 const DataSelection = { data: { event: true } } as const
 
 const processor = new SubstrateBatchProcessor()
     .setDataSource(config.dataSource)
+
     // Zenlink
     .addEvent('ZenlinkProtocol.LiquidityAdded', DataSelection)
     .addEvent('ZenlinkProtocol.LiquidityRemoved', DataSelection)
@@ -45,60 +42,6 @@ type Item = BatchProcessorItem<typeof processor>
 export type Ctx = BatchContext<Store, Item>
 
 processor.run(new TypeormDatabase(), async (ctx) => {
-    // TODO clean this up and move elsewhere
-    let transfersData = await getTransfers(ctx)
-
-    let accountIds = new Set<string>()
-    for (let t of transfersData) {
-        accountIds.add(t.from)
-        accountIds.add(t.to)
-    }
-
-    let accounts = await ctx.store
-        .findBy(Account, { id: In([...accountIds]) })
-        .then((accounts) => {
-            return new Map(accounts.map((a) => [a.id, a]))
-        })
-
-    let transfers: Transfer[] = []
-
-    for (let t of transfersData) {
-        let { id, blockNumber, timestamp, extrinsicHash, amount, fee } = t
-
-        let from = getAccount(accounts, t.from)
-        let to = getAccount(accounts, t.to)
-
-        transfers.push(
-            new Transfer({
-                id,
-                blockNumber,
-                timestamp,
-                extrinsicHash,
-                from,
-                to,
-                amount,
-                fee,
-            })
-        )
-    }
-
-    await ctx.store.save(Array.from(accounts.values()))
-    await ctx.store.insert(transfers)
-})
-
-interface TransferEvent {
-    id: string
-    blockNumber: number
-    timestamp: Date
-    extrinsicHash?: string
-    from: string
-    to: string
-    amount: bigint
-    fee?: bigint
-}
-
-async function getTransfers(ctx: Ctx): Promise<TransferEvent[]> {
-    let transfers: TransferEvent[] = []
     for (let block of ctx.blocks) {
         for (let item of block.items) {
             switch (item.name) {
@@ -180,33 +123,10 @@ async function getTransfers(ctx: Ctx): Promise<TransferEvent[]> {
                     })
                     break
                 case 'Balances.Transfer':
-                    // TODO maybe extract this to another function
-                    let e = new BalancesTransferEvent(ctx, item.event)
-                    let rec: {
-                        from: Uint8Array
-                        to: Uint8Array
-                        amount: bigint
-                    }
-                    if (e.isV1020) {
-                        let [from, to, amount] = e.asV1020
-                        rec = { from, to, amount }
-                    } else if (e.isV1050) {
-                        let [from, to, amount] = e.asV1050
-                        rec = { from, to, amount }
-                    } else if (e.isV9130) {
-                        rec = { ...e.asV9130 }
-                    } else {
-                        throw new Error('Unsupported spec')
-                    }
-                    transfers.push({
-                        id: item.event.id,
-                        blockNumber: block.header.height,
-                        timestamp: new Date(block.header.timestamp),
-                        extrinsicHash: item.event.extrinsic?.hash,
-                        from: ss58.codec('kusama').encode(rec.from),
-                        to: ss58.codec('kusama').encode(rec.to),
-                        amount: rec.amount,
-                        fee: item.event.extrinsic?.fee || 0n,
+                    await handleBalanceTransfer({
+                        ...ctx,
+                        block: block.header,
+                        event: item.event,
                     })
                     break
                 default:
@@ -214,15 +134,4 @@ async function getTransfers(ctx: Ctx): Promise<TransferEvent[]> {
             }
         }
     }
-    return transfers
-}
-
-function getAccount(m: Map<string, Account>, id: string): Account {
-    let acc = m.get(id)
-    if (acc == null) {
-        acc = new Account()
-        acc.id = id
-        m.set(id, acc)
-    }
-    return acc
-}
+})

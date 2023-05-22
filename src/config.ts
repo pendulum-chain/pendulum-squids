25a2e6c9bbbe9d580d9c70fda2b92ffd63823e12
Marcel Ebert <mail@marcel-ebert.de>
2023-05-22 18:11:12 +0200

    Add easy support for deploying same squid for Foucoco

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,8 @@
 import { ProcessorConfig } from './types'
 
-export const config: ProcessorConfig = {
+const network: string = process.env.NETWORK || 'amplitude'
+
+const amplitudeConfig: ProcessorConfig = {
     chainName: 'amplitude',
     prefix: 'amplitude',
     dataSource: {
@@ -8,3 +10,17 @@ export const config: ProcessorConfig = {
         chain: 'wss://rpc-amplitude.pendulumchain.tech',
     },
 }
+
+const foucocoConfig: ProcessorConfig = {
+    chainName: 'foucoco',
+    prefix: 'foucoco',
+    dataSource: {
+        archive: 'https://foucoco.archive.subsquid.io/graphql',
+        chain: 'wss://rpc-foucoco.pendulumchain.tech',
+    },
+}
+
+export const config: ProcessorConfig =
+    network === 'foucoco' ? foucocoConfig : amplitudeConfig
+
+console.log('Using ProcessorConfig: ', config)

b944baa27fdc8377240b329a6a36882b40e11d8c
adelarja <adelarja@gmail.com>
2023-08-02 15:11:15 -0300

    Draft for nabla events handlers

--- src/mappings/nabla.ts
+++ src/mappings/nabla.ts
@@ -2,6 +2,9 @@ import { EventHandlerContext } from '../types'
 import { network } from '../config'
 import * as ss58 from '@subsquid/ss58'
 import { toHex } from '@subsquid/util-internal-hex'
+import * as bpool from '../abi/backstop'
+import { BackstopPool, Router, NablaToken } from '../model'
+import * as erc20 from '../abi/erc20'
 
 function ss8ToHex(ss8Address: string[]) {
     var addresses = []
@@ -56,4 +59,64 @@ export async function handleContractEvent(ctx: EventHandlerContext) {
     } else if (ctx.event.args.address == MOCK_ORACLE_CONTRACT_ADDRESS) {
     } else if (ctx.event.args.address == MOCK_ERC20_CONTRACT_ADDRESS) {
     }
+}
+
+export async function getOrCreateBackstopPool(
+    ctx: EventHandlerContext,
+    address: string
+) {
+    let backstop = await ctx.store.get(BackstopPool, address)
+    if (!backstop) {
+        const contract = new bpool.Contract(ctx, address)
+        let router = getOrCreateRouter(ctx, toHex(await contract.router()))
+        const coverage = contract.coverage()
+        backstop = new BackstopPool({
+            id: BACKSTOP_POOL_CONTRACT_ADDRESS,
+            router: router,
+            token: await getOrCreateNablaToken(
+                ctx,
+                toHex(await contract.asset())
+            ),
+            totalSupply: await contract.totalSupply(),
+            reserves: coverage.get_reserves(),
+            liabilities: coverage.get_liabilities(),
+        })
+        ctx.store.save(backstop)
+    }
+    return backstop
+}
+
+export async function getOrCreateRouter(
+    ctx: EventHandlerContext,
+    address: string
+) {
+    let router = ctx.store.get(Router, address)
+    if (!router) {
+        router = new Router({
+            id: address,
+            swapPools: [],
+            backstopPools: [],
+            paused: false,
+        })
+        ctx.store.save(router)
+    }
+    return router
+}
+
+export async function getOrCreateNablaToken(
+    ctx: EventHandlerContext,
+    address: string
+) {
+    let nablaToken = ctx.store.get(NablaToken, address)
+    if (!nablaToken) {
+        const contract = new erc20.Contract(ctx, address)
+        nablaToken = new NablaToken({
+            id: address,
+            decimals: await contract.decimals(),
+            name: await contract.name(),
+            symbol: await contract.symbol(),
+        })
+        ctx.store.save(nablaToken)
+    }
+    return nablaToken
 }

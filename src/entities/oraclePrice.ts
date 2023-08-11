60c2ad620ba74b7ca9022daae4eb4b3aeedc1017
Marcel Ebert <mail@marcel-ebert.de>
2023-08-11 17:44:38 +0200

    Regenerate foucoco types and add OraclePrice entity

---
+++ src/entities/oraclePrice.ts
@@ -0,0 +1,46 @@
+import { TOKEN_METADATA_MAP, ZERO_BD } from '../constants'
+import { Token } from '../model'
+import { EventHandlerContext } from '../types'
+import {
+    addressFromAsset,
+    getTotalIssuance,
+    u8a2s,
+    zenlinkAssetIdToCurrencyId,
+} from '../utils/token'
+import { AssetId } from '../types/common'
+
+export async function getOrCreateOraclePrice(
+    ctx: EventHandlerContext,
+    asset: AssetId
+): Promise<Token | undefined> {
+    const address = addressFromAsset(asset)
+    let token = await ctx.store.get(Token, address)
+
+    if (!token) {
+        const metadata = TOKEN_METADATA_MAP[address]
+
+        if (!metadata) return undefined
+        const { name, symbol, decimals } = metadata
+        const totalSupply = await getTotalIssuance(
+            ctx,
+            zenlinkAssetIdToCurrencyId(asset)
+        )
+        token = new Token({
+            id: address.toLowerCase(),
+            name,
+            symbol,
+            totalSupply: totalSupply?.toString() ?? '0',
+            decimals,
+            derivedETH: ZERO_BD.toString(),
+            tradeVolume: ZERO_BD.toString(),
+            tradeVolumeUSD: ZERO_BD.toString(),
+            untrackedVolumeUSD: ZERO_BD.toString(),
+            totalLiquidity: ZERO_BD.toString(),
+            txCount: 0,
+        })
+
+        await ctx.store.save(token)
+    }
+
+    return token
+}
---
+++ src/model/generated/oraclePrice.model.ts
@@ -0,0 +1,48 @@
+import {
+    Entity as Entity_,
+    Column as Column_,
+    PrimaryColumn as PrimaryColumn_,
+    Index as Index_,
+} from 'typeorm'
+import * as marshal from './marshal'
+
+@Entity_()
+export class OraclePrice {
+    constructor(props?: Partial<OraclePrice>) {
+        Object.assign(this, props)
+    }
+
+    @PrimaryColumn_()
+    id!: string
+
+    @Index_()
+    @Column_('text', { nullable: false })
+    symbol!: string
+
+    @Column_('text', { nullable: false })
+    name!: string
+
+    @Index_()
+    @Column_('text', { nullable: false })
+    blockchain!: string
+
+    @Index_()
+    @Column_('numeric', {
+        transformer: marshal.bigintTransformer,
+        nullable: false,
+    })
+    timestamp!: bigint
+
+    /**
+     * BigDecimal
+     */
+    @Index_()
+    @Column_('text', { nullable: false })
+    price!: string
+
+    /**
+     * BigDecimal
+     */
+    @Column_('text', { nullable: false })
+    supply!: string
+}

0d4c187baa8630f79074cb04e6e8a9b64a608538
Marcel Ebert <mail@marcel-ebert.de>
2023-08-23 13:11:14 +0200

    Add other constants for price oracle

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,18 @@
 import { TokenBase } from './types'
 import { Big as BigDecimal } from 'big.js'
 
+// These are defined such that the data can be used to access the data from the on-chain price oracle
+// The key is the blockchain-symbol pair, and the value is the Zenlink asset ID
 export const PRICE_ORACLE_KEYS_TO_ADDRESS: { [address: string]: string } = {
+    // On Amplitude (capitalization matters)
     'Amplitude-AMPE': '2124-0-0',
+    // On Foucoco (capitalization matters)
+    'AMPLITUDE-AMPE': '2124-0-0',
     'Kusama-KSM': '2124-2-256',
+    'Stellar-XLM': '2124-2-512',
+    'FIAT-USD-USD': '2124-2-513',
+    'FIAT-TZS-USD': '2124-2-514',
+    'FIAT-BRL-USD': '2124-2-515',
 }
 
 // These are defined such that the data can be used to access the data from the on-chain price oracle
@@ -18,6 +27,30 @@ export const TOKEN_METADATA_MAP: { [address: string]: TokenBase } = {
         name: 'Kusama',
         symbol: 'KSM',
         blockchain: 'Kusama',
+        decimals: 12,
+    },
+    '2124-2-512': {
+        name: 'Stellar Native',
+        blockchain: 'Stellar',
+        symbol: 'XLM',
+        decimals: 12,
+    },
+    '2124-2-513': {
+        name: 'Stellar USDC',
+        blockchain: 'Stellar',
+        symbol: 'USDC',
+        decimals: 12,
+    },
+    '2124-2-514': {
+        name: 'Stellar TZS',
+        blockchain: 'Stellar',
+        symbol: 'TZS',
+        decimals: 12,
+    },
+    '2124-2-515': {
+        name: 'Stellar BRL',
+        blockchain: 'Stellar',
+        symbol: 'BRL',
         decimals: 12,
     },
 }

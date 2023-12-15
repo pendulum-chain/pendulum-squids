d2e7589417c48a046fcb1b40d14247ce01d4cfed
Gianfranco Tasteri <gianfra33@gmail.com>
2023-12-15 16:58:23 -0300

    WIP use dynamic event and storage decoder

--- src/mappings/prices.ts
+++ src/mappings/prices.ts
@@ -3,10 +3,16 @@ import { foucocoEvents, amplitudeEvents, pendulumEvents } from '../types/events'
 import { network } from '../config'
 import { getOrCreateOraclePrice } from '../entities/oraclePrice'
 import { hexToString } from '@polkadot/util'
+import { decodeFoucocoEvent } from '../types/foucoco/eventsDecoder'
 export async function handleUpdatedPrices(ctx: EventHandlerContext) {
     let event
     if (network === 'foucoco') {
-        event = foucocoEvents.diaOracleModule.updatedPrices.v1.decode(ctx.event)
+        event = await decodeFoucocoEvent(
+            'diaOracleModule',
+            'updatedPrices',
+            ctx
+        )
+        console.log(event)
     } else if (network === 'pendulum') {
         event = pendulumEvents.diaOracleModule.updatedPrices.v3.decode(
             ctx.event

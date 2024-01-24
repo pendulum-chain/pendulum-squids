7c9e2afcb58972dc59b8930777af8ffd7532c743
Marcel Ebert <mail@marcel-ebert.de>
2024-01-24 19:09:00 +0100

    Fix parsing and and start analysing KSM-USD prices

--- src/mappings/prices.ts
+++ src/mappings/prices.ts
@@ -3,18 +3,28 @@ import { network } from '../config'
 import { getOrCreateOraclePrice } from '../entities/oraclePrice'
 import { hexToString } from '@polkadot/util'
 import { decodeEvent } from '../types'
-import { parseFromCSV } from '../utils/ohlcv-parsers'
+import { getOHLCVAtTime, readCSV } from '../utils/ohlcv-parsers'
 
 const TIMEFRAME_INTERVAL_IN_MINUTES = 1
-const KrakenXlmUsd = parseFromCSV(
-    `ohlcv/XLMUSD_${TIMEFRAME_INTERVAL_IN_MINUTES}.csv`
-)
-const KrakenKsmUsd = parseFromCSV(
+const KrakenKsmUsd = readCSV(
     `ohlcv/KSMUSD_${TIMEFRAME_INTERVAL_IN_MINUTES}.csv`
 )
-const KrakenDotUsd = parseFromCSV(
-    `ohlcv/DOTUSD_${TIMEFRAME_INTERVAL_IN_MINUTES}.csv`
-)
+
+interface AnalysisResult {
+    priceDiff: number
+    priceDiffPercentage: number
+    timestamp: number
+}
+
+// Map of number to AnalysisResult
+const KsmUsdAnalysis: { [timestamp: number]: AnalysisResult } = {}
+
+// const KrakenXlmUsd = parseFromCSV(
+//     `ohlcv/XLMUSD_${TIMEFRAME_INTERVAL_IN_MINUTES}.csv`
+// )
+// const KrakenDotUsd = parseFromCSV(
+//     `ohlcv/DOTUSD_${TIMEFRAME_INTERVAL_IN_MINUTES}.csv`
+// )
 
 export async function handleUpdatedPrices(ctx: EventHandlerContext) {
     let event = await decodeEvent(
@@ -43,6 +53,42 @@ export async function handleUpdatedPrices(ctx: EventHandlerContext) {
         oraclePrice.price = coinInfo.price.toString()
         oraclePrice.supply = coinInfo.supply.toString()
         oraclePrice.timestamp = coinInfo.lastUpdateTimestamp
+
+        // Check against the Kraken price
+        if (blockchain === 'Kusama' && symbol === 'KSM') {
+            const timestamp = Number(oraclePrice.timestamp)
+            try {
+                const data = await KrakenKsmUsd
+                const ohlcvAtTime = getOHLCVAtTime(data, timestamp)
+                const krakenPrice = ohlcvAtTime.close
+                let diaPrice = Number(oraclePrice.price)
+                // We downscale the price by 12 decimals
+                diaPrice = diaPrice / 10 ** 12
+                const priceDiff = Math.abs(krakenPrice - diaPrice)
+                const priceDiffPercentage = priceDiff / krakenPrice
+                if (priceDiffPercentage > 0.1) {
+                    console.log(
+                        `KSM price difference is ${priceDiffPercentage * 100}%`
+                    )
+                }
+                console.log(
+                    'Comparing KSM price to Kraken price',
+                    krakenPrice,
+                    diaPrice,
+                    priceDiff,
+                    priceDiffPercentage,
+                    'at timestamp',
+                    timestamp
+                )
+                KsmUsdAnalysis[timestamp] = {
+                    priceDiff,
+                    priceDiffPercentage,
+                    timestamp,
+                }
+            } catch (e) {
+                console.log('Error getting KSM price', e)
+            }
+        }
 
         await ctx.store.save(oraclePrice)
     }
--- src/utils/ohlcv-parsers.ts
+++ src/utils/ohlcv-parsers.ts
@@ -1,8 +1,8 @@
-import fs from 'fs'
+import { promises as fs } from 'fs'
 import { parse } from 'csv-parse'
 
 interface OHLCV {
-    time: Date
+    time: number
     open: number
     high: number
     low: number
@@ -13,50 +13,47 @@ interface OHLCV {
 const OHLCV_FIELDS = ['time', 'open', 'high', 'low', 'close', 'volume']
 
 // A function that reads a string from a csv file
-function readCSV(path: string): string {
-    let content = ''
-    fs.createReadStream(path)
-        .pipe(parse({ delimiter: ',', fromLine: 2 }))
-        .on('data', function (row) {
-            content += row
-        })
-        .on('end', function () {
-            console.log('done parsing csv', path)
-        })
-        .on('error', function (error) {
-            console.log('error parsing csv', path, error)
-        })
-    return content
-}
+export async function readCSV(path: string): Promise<OHLCV[]> {
+    const content = await fs.readFile(path)
+    return new Promise((resolve, reject) => {
+        parse(content, { cast: true }, (err, records: Array<number[]>) => {
+            if (err) {
+                reject(err)
+            }
+            // Convert the records to OHLCV objects
+            const ohlcvs: OHLCV[] = records.map((record: number[]) => {
+                const ohlcv: OHLCV = {} as OHLCV
+                for (let i = 0; i < OHLCV_FIELDS.length; i++) {
+                    const field = OHLCV_FIELDS[i]
+                    // @ts-ignore
+                    ohlcv[field] = Number(record[i])
+                }
+                return ohlcv
+            })
 
-// A function that takes a csv string and returns an array of OHLCV objects
-// The csv string is the raw data from the exchange
-export function parseFromCSV(csvPath: string): OHLCV[] {
-    const csv = readCSV(csvPath)
-
-    const lines = csv.split('\n')
-    const ohlcv = []
-    for (let i = 1; i < lines.length; i++) {
-        const line = lines[i]
-        const [time, open, high, low, close, volume] = line.split(',')
-        ohlcv.push({
-            time: new Date(parseInt(time)),
-            open: parseFloat(open),
-            high: parseFloat(high),
-            low: parseFloat(low),
-            close: parseFloat(close),
-            volume: parseFloat(volume),
+            resolve(ohlcvs)
         })
-    }
-    return ohlcv
+    })
 }
 
 // A function that takes a timestamp and returns the OHLCV object for that timestamp
 // The timestamp is the timestamp of the candle
-export function getOHLCVAtTime(ohlcv: OHLCV[], timestamp: Date): OHLCV {
-    const index = ohlcv.findIndex(
-        (candle) => candle.time.getTime() === timestamp.getTime()
-    )
+export function getOHLCVAtTime(ohlcv: OHLCV[], timestamp: number): OHLCV {
+    // Go through the list and return the index of the timestamp that is closest to the timestamp we are looking for
+    // because we cannot be sure to find the exact same timestamp in the list
+    let index = -1
+    for (let i = 0; i < ohlcv.length; i++) {
+        const candle = ohlcv[i]
+        if (candle.time === timestamp) {
+            index = i
+            break
+        }
+        if (candle.time > timestamp) {
+            index = i - 1
+            break
+        }
+    }
+
     if (index === -1) {
         throw new Error(`OHLCV for timestamp ${timestamp} not found`)
     }
